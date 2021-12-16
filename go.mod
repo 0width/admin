a664746f7a8490355264fbe63575b3e31e34d575
xios <xios-young@qq.com>
2021-12-16 11:56:57 +0800

    add active

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.15
 require (
 	git.xios.club/xios/gc v0.0.0-20211215063022-e8a8eab33164
 	github.com/gin-gonic/gin v1.7.7
-	golang.org/x/tools v0.1.8 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gorm.io/driver/mysql v1.2.1
 	gorm.io/gorm v1.22.4
 )
--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"git.xios.club/xios/gc"
-	"github.com/gin-gonic/gin"
-
 	_ "admin/business/pogo/entity"
 	_ "admin/component/db/mysql"
+
+	"git.xios.club/xios/gc"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	server := gin.Default()
 
 	gc.RegisterBean(&server)
-	gc.LoadProperties("./config/application.yml")
+
+	confDir := "./config"
+
+	gc.LoadProperties(confDir + "/application.yml")
+	if confActive := gc.GetStringProperty("active"); confActive != "" {
+		gc.LoadProperties(confDir + "/application-" + confActive + ".yml")
+	}
 	gc.AutoWireBeans()
 
 	_ = server.Run(":8080")
