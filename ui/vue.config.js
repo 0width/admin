f809da366fb51d60f51d4c7e7b450ddb4b41a1ef
xios <xios-young@qq.com>
2021-12-21 08:03:03 +0800

    更改打开浏览器的方式

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -31,8 +31,7 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     port: port,
-    open: true,
-    disableHostCheck: true,
+    open: false,
     overlay: {
       warnings: false,
       errors: true
